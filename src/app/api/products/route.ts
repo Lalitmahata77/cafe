80f008ea61a651685d8107e3ad1a94084931425b
Lalit-mahata <lm3654725@gmail.com>
2025-02-17 00:02:38 +0545

    get products and single product

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/lib/db/db";
 import { products } from "@/lib/db/schema";
 import { isServer, productSchema } from "@/lib/validator/productSchema";
+import { desc } from "drizzle-orm";
 import { unlink, writeFile } from "node:fs/promises";
 import path from "node:path";
 
@@ -47,4 +48,17 @@ export async function POST(request: Request){
         
     }
     return Response.json({ message: 'OK' }, { status: 201 });
+}
+
+
+export async function GET(){
+    try {
+        const allProducts = await db.select().from(products).orderBy(desc(products.id))
+       return Response.json(allProducts)
+        
+    } catch (error) {
+        console.log(error);
+        return Response.json({message : "Failed to fetch data"}, {status:500})
+        
+    }
 }
